aed473511ff40ae8fab685b2be6cef666c605335
jbstuart01 <145608139+jbstuart01@users.noreply.github.com>
2024-05-08 15:36:47 -0400

    Run uvicorn in browser

--- app.py
+++ app.py
@@ -6,24 +6,24 @@ app = FastAPI()
 # calculate compound interest given an initial investment, monthly contribution,
 # rate of return, compounds per year, and total years of investing
 @app.get("/calculate")
-def calculate_model(initial_inv: int, monthly_cont: int, rate: float, annual_comps: int, years: int):
-    monthly_rate = rate / 100 / annual_comps
-
-    # calculate the total number of compounding periods
-    total_periods = annual_comps * years
+def calculate_model(initial_inv: int, monthly_cont: int, rate: float, years: int):
+    monthly_rate = rate / 100
 
     # initialize future value to the initial investment amount
     future_value = initial_inv
 
     # apply compound interest formula for each period
-    for i in range(total_periods):
+    for i in range(years):
+        # add on this year's contributions
+        future_value += (monthly_cont * 12)
         # the future value is incremented by multiplying 1 + the monthly rate
         future_value *= (1 + monthly_rate)
-        # add on this month's contribution
-        future_value += monthly_cont
 
     # return the final value of interest compounded over the given period
-    return {'value' : future_value}
+    return {'final_value' : round(future_value, 2)}
 
+def main():
+    calculate_model(25000, 750, 10, 35)
 
-
+if __name__ == "__main__":
+    main()
